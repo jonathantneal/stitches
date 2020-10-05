33d23e73ac0004abfdc7338c65eea2df57011979
github-action-benchmark <github@users.noreply.github.com>
2020-10-05 23:46:40 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 6cf5a22b419999409b0d51f9c35b36da9ab04c0c

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1601941600869,
+  "repoUrl": "https://github.com/jonathantneal/stitches",
+  "entries": {
+    "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "jonathantneal",
+            "username": "jonathantneal"
+          },
+          "committer": {
+            "name": "jonathantneal",
+            "username": "jonathantneal"
+          },
+          "id": "6cf5a22b419999409b0d51f9c35b36da9ab04c0c",
+          "message": "Create main.yml",
+          "timestamp": "2020-10-05T23:09:52Z",
+          "url": "https://github.com/jonathantneal/stitches/pull/1/commits/6cf5a22b419999409b0d51f9c35b36da9ab04c0c"
+        },
+        "date": 1601941600186,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Benchmark 1",
+            "value": 14259113,
+            "range": "±1.16%",
+            "unit": "ops/sec",
+            "extra": "73 samples"
+          },
+          {
+            "name": "Benchmark 2",
+            "value": 2113974,
+            "range": "±1.97%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          }
+        ]
+      }
+    ]
+  }
+}

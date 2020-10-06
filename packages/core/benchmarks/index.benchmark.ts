81afa3a88cc196931a3da19d840a85f30a9401f0
Jonathan Neal <jonathantneal@hotmail.com>
2020-10-06 11:54:15 -0400

    Add initial createCss benchmarks

--- packages/core/benchmarks/index.benchmark.ts
+++ packages/core/benchmarks/index.benchmark.ts
@@ -1,11 +1,35 @@
-import { tokenizeValue } from '../src/shorthand-parser/value-tokenizer';
+import { createCss, createTokens, hotReloadingCache } from '../src';
 
-describe('Tokenizing', () => {
-  test('Undefined', () => {
-    tokenizeValue(undefined as any);
+describe('createCss', () => {
+  test('create simple atoms', () => {
+    const css = createCss({}, null);
+    const atoms = css({ color: 'red' }) as any;
+    const atom = atoms.atoms[0];
+
+    const { styles } = css.getStyles(() => {
+      expect(atom.toString()).toMatchInlineSnapshot(`"_vfarC"`);
+
+      return '';
+    });
   });
 
-  test('Two Ident Tokens', () => {
-    tokenizeValue('solid red');
+  test('compose atoms', () => {
+    const css = createCss({}, null);
+    css({ color: 'red', backgroundColor: 'blue' }).toString();
+  });
+
+  test('create tokens', () => {
+    const tokens = createTokens({
+      colors: {
+        RED: 'tomato',
+      },
+    });
+    const css = createCss({ tokens }, null);
+    const atom = (css({ color: 'RED' }) as any).atoms[0];
+
+    const { styles } = css.getStyles(() => {
+      atom.toString();
+      return '';
+    });
   });
 });

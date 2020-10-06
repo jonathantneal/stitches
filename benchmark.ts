9af460efb21eddf98224ba780c04bd5ce11f7d7c
Jonathan Neal <jonathantneal@hotmail.com>
2020-10-06 12:52:55 -0400

    Update benchmarks and benchmarks script

--- benchmark.ts
+++ benchmark.ts
@@ -2,27 +2,53 @@ import Benchmark from 'benchmark';
 import glob from 'tiny-glob';
 import { resolve } from 'path';
 
-/** Benchmark suites to be added by benchmark files. */
-const suites: Set<Benchmark.Suite> = new Set();
+type BenchmarksState = {
+  /** Benchmark suites to be added by benchmark files. */
+  allBenchmarks: Set<BenchmarkState>;
+
+  /** Current benchmark being evaluated. */
+  currentBenchmark: BenchmarkState | null;
+};
+
+type BenchmarkState = {
+  /** Previous benchmark suite being evaluated. */
+  previousBenchmark: BenchmarkState | null;
+
+  /** Current benchmark suite being evaluated. */
+  currentSuite: Benchmark.Suite;
+};
+
+const benchmarksState: BenchmarksState = {
+  allBenchmarks: new Set(),
+  currentBenchmark: null,
+};
 
 // define a global `describe` function that maps to BenchmarkJS
 Reflect.set(globalThis, 'describe', (name: string, fn: Function) => {
-  /** Benchmark suite. */
-  const suite = new Benchmark.Suite(name);
-  suite.on('cycle', (event: { target: string }) => console.log(`${name}: ${event.target}`));
-  suites.add(suite);
-
-  // preserve global `test`
-  const preservedGlobalTest = Reflect.get(globalThis, 'test');
-
-  // define a global `test` function that maps to this benchmark suite
-  Reflect.set(globalThis, 'test', suite.add.bind(suite));
-
-  // run this benchmark suite initializing function.
+  const benchmark: BenchmarkState = {
+    previousBenchmark: benchmarksState.currentBenchmark,
+    currentSuite: new Benchmark.Suite(name),
+  };
+  benchmark.currentSuite.on('cycle', (event: { target: string }) => console.log(`${name}: ${event.target}`));
+  benchmarksState.currentBenchmark = benchmark;
+  benchmarksState.allBenchmarks.add(benchmark);
   fn();
+  benchmarksState.currentBenchmark = benchmarksState.currentBenchmark?.previousBenchmark || null;
+});
 
-  // Restore global `test`
-  Reflect.set(globalThis, 'test', preservedGlobalTest);
+// define a global `test` function that maps to BenchmarkJS
+Reflect.set(globalThis, 'test', (name: string, fn: Function) => {
+  if (benchmarksState.currentBenchmark) {
+    benchmarksState.currentBenchmark.currentSuite.add(name, fn);
+  } else {
+    const benchmark: BenchmarkState = {
+      previousBenchmark: benchmarksState.currentBenchmark,
+      currentSuite: new Benchmark.Suite(name),
+    };
+    benchmark.currentSuite.on('cycle', (event: { target: string }) => console.log(`${event.target}`));
+    benchmark.currentSuite.add(name, fn);
+    benchmarksState.allBenchmarks.add(benchmark);
+  }
 });
 
 // collect all benchmark tests
@@ -30,4 +56,4 @@ glob('**/*.benchmark.ts')
   // prepare all benchmark tests
   .then((files: string[]) => Promise.all(files.map((file) => import(resolve(file)))))
   // run all benchmark tests
-  .then(() => suites.forEach((suite) => suite.run()));
+  .then(() => benchmarksState.allBenchmarks.forEach(({ currentSuite }) => currentSuite.run()));
